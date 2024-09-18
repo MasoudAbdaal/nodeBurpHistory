7d8ee175b3b0a95f9e96e9678faf14cdd75bd3fb
Masoud Abdaal <m_abdaal@outlook.com>
2024-09-18 20:35:19 +0330

    Added interfaces; Fix extensions, mimetype bugs

---
+++ DataStructure.ts
@@ -0,0 +1,41 @@
+type HTTPMethods = "GET" | "POST" | "PUT" | "OPTIONS" | "CONNECT" | "TRACE";
+
+type ShapeType = "String" | "Boolean" | "Regex" | "Integer";
+
+export interface IShape {
+  value: string
+  type: ShapeType | string;
+  required: boolean;
+  min: number;
+  max: number;
+}
+
+interface IFeatures {
+  comment: object;
+  uploads: ["file_sharing", "profile"];
+  integrations: {
+    auth: ["  google,facebook,youtube"];
+  };
+}
+
+export interface IEndPoint {
+  [name: string]: {
+    url: string;
+    method: HTTPMethods | string;
+    shape: { [name: string]: IShape };
+    sampleData?: string;
+  };
+}
+
+const info: IEndPoint = {
+  otp: {
+    url: "/api/voyager/C/CustomerAccount/OTPRegister",
+    method: "POST",
+    shape: {
+      mobile: { type: "String", required: true, max: 12, min: 12 },
+      confirmTerms: { type: "Boolean", required: true, max: 0, min: 0 },
+      notRobot: { type: "Boolean", required: true, max: 0, min: 0 },
+    },
+    sampleData: '{"mobile":"09333039702","confirmTerms":true,"notRobot":false}',
+  },
+};
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,81 @@
 import { Parser } from "xml2js";
 import * as fs from "fs";
 
-import { IXMLRoot } from "./types/IRoot";
+import { IXMLEntity } from "./types/IXMLEntity";
+import { IEndPoint, IShape } from "../DataStructure";
+import { log } from "console";
 
 function readFile(): void {
   const parser = new Parser();
-  let root: IXMLRoot | undefined;
+  // let item: IXMLEntity | undefined;
+
   fs.readFile(String(process.env.BURP_HISTORY_FILE_PATH), (er, data) => {
+    let finalEntity: IEndPoint = {};
     parser.parseString(data, (err, res) => {
       if (err) {
-        return console.log(err);
+        return console.log(err?.message);
       }
-      root = res.items.item[0];
-      const responsed: any = atob(root?.request[0]._!);
-      console.debug(root?.protocol.join(""));
-      console.debug(responsed);
+
+      res.items.item.forEach((item: IXMLEntity) => {
+        let addType;
+        try {
+          if (item.mimetype != undefined) {
+            addType = item.mimetype![0];
+          } else {
+            addType = item.type![0];
+          }
+
+          if (
+            item.method[0] !== "OPTIONS" &&
+            (addType == "JSON" || addType == "" || item.extension[0] == "null")
+          ) {
+            const request = atob(item?.request[0]._!);
+
+            const reqBody = request.split("\r\n");
+            const shapes: { [name: string]: IShape } = {};
+
+            if (reqBody[reqBody.length - 1] != "") {
+              try {
+                const finalBody = JSON.parse(reqBody[reqBody.length - 1]);
+
+                Object.keys(finalBody).forEach((x) => {
+                  const leng = String(finalBody[x]).length;
+
+                  shapes[x] = {
+                    value: finalBody[x],
+                    max: 0,
+                    min: leng,
+                    required: true,
+                    type: typeof finalBody[x],
+                  };
+                });
+              } catch (e) {
+                console.log(e);
+              }
+            }
+
+            const url = item?.path[0]!;
+
+            finalEntity[url] = {
+              url: url,
+              method: item?.method[0]!,
+              shape: shapes,
+            };
+          }
+        } catch (e) {
+          console.error(
+            "method:",
+            item.method[0],
+            item.url[0],
+            "\n",
+            "extension:",
+            item.extension[0],
+            e
+          );
+        }
+      });
     });
+    console.log(finalEntity);
   });
 }
 
